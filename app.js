c8088f5fe6e0377330dae0d1b91c996bef9dbbd4
Bibin <bibinlopez36@gmail.com>
2023-04-11 19:16:54 +0530

    Numbers

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+// Numbers
+// don't declare type
+
+const number = 34;
+// let pants = 2.466;
+// pants = 'are great'
+
+const number2 = 2.456
+const number3 = '2.456'
+console.log(number);
+console.log(number2);
+console.log(number3);
