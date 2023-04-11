8cc5338591c44673b99350add5a657a004333096
Bibin <bibinlopez36@gmail.com>
2023-04-11 20:49:21 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+// Arrays, Functions and Objects
+// Arrays - [] , 0 index based
+
+const friends1 = 'anna';
+const friends2 = 'anna';
+const friends3 = 'anna';
+const friends4 = 'anna';
+
+const friends = ['john', 'peter', 'bob', 'susy', 45, undefined, null]
+
+let bestFriend = friends[2]
+friends[4] = 'anna'
+console.log(bestFriend);
+
+console.log(friends);
+console.log(friends[3]);
+console.log(friends.length);   
+
+// we can store list of items
+// it is 0 index based
+// we access value using indexes
+// we re-assign value using indexes
+// indes represents items in the array , otherwise you are going to get undefined
