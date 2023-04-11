ff473e0e1cd1e6155cb1f5e09fe976cb7f41c896
Bibin <bibinlopez36@gmail.com>
2023-04-11 19:55:45 +0530

    Number ramaining

---
+++ number2.js
@@ -0,0 +1,33 @@
+
+// Numbers
+// +=, -= , *= , ++ , -- , %
+// Modulus (%) operator returns the remaider after integer division
+
+let number = 60 ;
+number += 5;
+number += 5;
+number += 5;
+number -= 5;
+number += 1;
+number += 1;
+number += 1;
+number += 1;
+number--;
+number--;
+number--;
+number--;
+
+console.log(number); 
+
+
+const slices = 11;
+const children = 3;
+const amount = slices % children ;
+
+console.log ( amount)
+
+const random = 4 + 6 + 10 * 10 ; 
+const random2 = (4 +  6 + 10) * 10;
+
+console.log(random);
+console.log(random2);
