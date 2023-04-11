382a8b1455192796acbfaa953ee1aaf91b3a29c4
Bibin <bibinlopez36@gmail.com>
2023-04-11 21:04:04 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,23 @@
+// Arrays , Functions and Objects
+// Functions - declare ,invoke
+// params - when declare/define
+// placeholders, local vars
+// arguments - when invoke/call/run
+// use vars/values, multiple params, undefined
+
+const bob = 'Bob';
+const susy = 'Susy';
+const anna = 'Anna'
+
+
+function greet(name,second){
+  console.log(second);
+  console.log('Hello there ' + name)
+}
+
+// greet bob
+greet(4)
+// greet anna
+greet(anna, bob)
+//greet susy
+greet('Susy')
