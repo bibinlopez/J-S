0f4afc5b8ffc312d7dc7669e9fff230769242cfc
Bibin <bibinlopez36@gmail.com>
2023-04-14 13:08:29 +0530

    condi. statements

---
+++ conditional statements.js
@@ -0,0 +1,54 @@
+// Conditional Statements 
+// Comparison Operators
+// >, < , >= , <=, ==, ===, !=, !==
+// else if and !
+
+// if(true){
+//   console.log('hello world')
+// }
+
+// const value = true;
+const value = 3 > 2
+console.log(typeof value);
+
+// if (value) {
+//   console.log('hello world')
+// }
+
+const value2 = false;
+if (value2) {
+  console.log('hello world');
+} else {
+  console.log('hello people');
+}
+
+
+// const num1 = 3 ; 
+// const num2 = 3 ;
+// if (num1 > num2) {
+//   console.log('first number is bigger than second');
+// } else {
+//   console.log('second number is bigger than first');
+// }
+
+// const num1 = 3;
+// const num2 = 3;
+// const result = num1 >= num2
+// if (num1 > num2) {
+//   console.log('first number is bigger than second');
+// } else if (result) {
+//   console.log('first number is equal to second');
+// } else {
+//   console.log('second number is bigger than first');
+// }
+
+
+const num1 = 3;
+const num2 = 3;
+const result = num1 >= num2
+
+const number = true ;
+
+if(!number){
+  console.log('value is true');
+}
