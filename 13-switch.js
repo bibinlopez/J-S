4c0b5c31e7f9883bafa731323ba55e2f53312007
Bibin <bibinlopez36@gmail.com>
2023-04-14 13:35:36 +0530

    switch

---
+++ 13-switch.js
@@ -0,0 +1,26 @@
+// Switch 
+// dice values : 1 - 6
+
+
+const dice = 2;
+
+switch (dice) {
+  case 1:
+    console.log('you got one');
+    break;
+  case 2:
+    console.log('you got two');
+    break;
+  case 3:
+    console.log('you got three');
+    break;
+  case 4:
+    console.log('you got four');
+    break;
+  case 5:
+    console.log('you got five');
+    break;
+  case 6:
+    console.log('you got six');
+    break;
+}
