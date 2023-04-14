3c6f87ad710e0a4631cfded59a14fbe47e5ff94d
Bibin <bibinlopez36@gmail.com>
2023-04-14 13:28:50 +0530

    logical operator

---
+++ 12-logical operator.js
@@ -0,0 +1,17 @@
+// Logical Operators
+// (|| - OR), (&& - AND), !
+
+const name = 'peter';
+const age = 30;
+
+// if(name !== 'bob' || age < 21) {
+//   console.log('hello there user');
+// } else {
+//   console.log('wrong values');
+// }
+
+if(name !== 'bob' && age < 21) {
+  console.log('hello there user');
+} else {
+  console.log('wrong values');
+} 
