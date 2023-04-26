7a4e9f5b9f1dce4e6acb2c07e0939b1ff8893d40
Bibin <bibinlopez36@gmail.com>
2023-04-26 13:16:28 +0530

    directory change

---
+++ Basics/14-while.js
@@ -0,0 +1,10 @@
+// Loops
+// repeatedly run a block of code while condition is true
+// while loop
+
+const amount = 10;
+
+while(amount > 0){
+  console.log(`I have ${amount} dollars and I'm going to the mall`);
+  
+}
---
+++ Basics/16-loop.js
@@ -0,0 +1,12 @@
+// Loops
+// repeatedly run a block of code while condition is true
+// for loop
+
+// let i;
+// for (i = 0 ; i< 10 ; i++){
+//   console.log('and the number is: '+ i);
+// }
+
+for(let i = 11 ; i >+10 ; i --){
+  console.log('and the number is : ' + i);
+}
