88e5c69461a729114d0945ee4ff45a8bac562aed
Bibin <bibinlopez36@gmail.com>
2023-04-28 15:50:16 +0530

    array and for loop

---
+++ arrayForLoop.js
@@ -0,0 +1,19 @@
+// Arrays and for loop
+
+const names = ['anna', 'susy', 'bob'];
+const lastName = 'shakeandbake';
+let newArray = [];
+
+//for loop
+for (let i = 0; i < names.length; i++) {
+  console.log(i);
+  console.log(names[i]);
+  // newArray.push(names[i])
+  const fullName = `${names[i]} ${lastName}`
+  newArray.push(fullName)
+  // newArray.push(`${names[i]} ${lastName}`)
+}
+
+
+console.log(names);
+console.log(newArray);
---
+++ stringMethods.js
@@ -0,0 +1,16 @@
+// String properties and methods
+// wrapper Stirng Object, don't memorize methods
+
+let text = ' Peter Jordan';
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.charAt(1));
+console.log(text.charAt(12));
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf('P'));
+console.log(text.trim());
+console.log(text.trim().toLowerCase().startsWith('peter'));
+console.log(text.includes('eter'));
+console.log(text.slice(0, 2));
+console.log(text.slice(-2)); 
